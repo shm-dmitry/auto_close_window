51a763c0009b4dd3ea76a5d2e2111785ef0aeeb0
dmitry <shm-dmitry@yandex.ru>
2021-08-17 14:41:10 +0300

    actualized schematics of outdoor noise sensor module

--- avr/sensor/noise.ino
+++ avr/sensor/noise.ino
@@ -1,6 +1,6 @@
-#define NOISE_STROBE_PIN PIN_PA2
-#define NOISE_RESET_PIN PIN_PA0
-#define NOISE_OUT_PIN PIN_PA1
+#define NOISE_STROBE_PIN PIN_PA6
+#define NOISE_RESET_PIN PIN_PA7
+#define NOISE_OUT_PIN PIN_PA5
 
 void noise_init() {
   pinMode(NOISE_STROBE_PIN, OUTPUT);
@@ -20,18 +20,18 @@ void noise_init() {
 
 bool noise_read_level(uint16_t * buffer) {
   digitalWrite (NOISE_RESET_PIN,  HIGH);
-  delayMicroseconds(1);
+  delayMicroseconds(300);
   digitalWrite (NOISE_RESET_PIN,  LOW);
-  delayMicroseconds(1);
+  delayMicroseconds(300);
 
   for (int i = 0; i < 7; i++) {
     digitalWrite(NOISE_STROBE_PIN, HIGH);
-    delayMicroseconds(35);
+    delayMicroseconds(300);
     digitalWrite(NOISE_STROBE_PIN, LOW);
 
     buffer[i] = analogRead(NOISE_OUT_PIN);
 //    buffer[i] = map(analogRead(NOISE_OUT_PIN), 0, 1023, 0, 255);
 
-    delayMicroseconds(72);
+    delayMicroseconds(300);
   }
 }
--- avr/sensor/sensor.ino
+++ avr/sensor/sensor.ino
@@ -1,12 +1,12 @@
 void setup() {
   gpio_logger_init();
-//  noise_init();
+  noise_init();
 //  bmp280_initialize();
-  sgp30_initialize();
+//  sgp30_initialize();
 }
 
 void loop() {
-  test_sgp30();
+  test_noise_level();
   delay(1000);
 }
 
@@ -23,6 +23,7 @@ void test_noise_level() {
     uint16_t noise_level[7] = {0xFFFF,0xFFFF,0xFFFF,0xFFFF,0xFFFF,0xFFFF,0xFFFF};
     noise_read_level(noise_level);
     for (int i = 0; i<7; i++) {
-      gpio_logger_send_message("N%i = %04X", i, noise_level[i]);
+      gpio_logger_send_message("N%i = %u", i, noise_level[i]);
     }
+    gpio_logger_send_message("------");
 }

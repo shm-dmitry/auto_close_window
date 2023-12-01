7387d8c353f413e95dffdfbae5ec2c474e389398
dshmyglev <d.shmyglev@plati-perevodi.ru>
2023-12-01 11:26:31 +0300

    assigned different addresses

--- src/main_module/main/controller/controller.c
+++ src/main_module/main/controller/controller.c
@@ -16,24 +16,12 @@
 #define CONTROLLER_PDU_ARG_STEP_CLOSE  0x61
 
 #define CONTROLLER_HM_FULL_OPEN		   0x01
-#define CONTROLLER_HM_FULL_CLOSE		   0x02
+#define CONTROLLER_HM_FULL_CLOSE	   0x02
 #define CONTROLLER_HM_STEP_OPEN		   0x03
-#define CONTROLLER_HM_STEP_CLOSE		   0x04
+#define CONTROLLER_HM_STEP_CLOSE	   0x04
 #define CONTROLLER_HM_CALIBRATE		   0x05
-#define CONTROLLER_HM_ON_CHARGE_BEGIN	   0x06
+#define CONTROLLER_HM_ON_CHARGE_BEGIN  0x06
 #define CONTROLLER_HM_ON_CHARGE_END	   0x07
-
-#define CONTROLLER_OM_ON_CHARGE_BEGIN	   0x51
-#define CONTROLLER_OM_ON_CHARGE_END	   0x52
-#define CONTROLLER_OM_STATUS_AIR          0x53
-#define CONTROLLER_OM_STATUS_NOISE_ALRM   0x54
-#define CONTROLLER_OM_STATUS_NOISE_DATA   0x55
-
-#define CONTROLLER_MM_ON_STATUS   	       0xA0
-
-void controller_on_air_data(int8_t temperature, uint8_t humidity, uint32_t pressure);
-void controller_on_noise_alarm(bool alarm);
-void controller_on_noise_data(uint8_t freq1, uint8_t level1, uint8_t freq2, uint8_t level2, uint8_t freq3, uint8_t level3, uint8_t freq4, uint8_t level4);
 
 void controller_process_pdu_command(uint8_t arg) {
 	ESP_LOGI(LOG_CONTROLLER, "PDU command: arg=%02X", arg);
@@ -54,50 +42,79 @@ void controller_process_pdu_command(uint8_t arg) {
 	}
 }
 
-void controller_process_internal_command(uint16_t address, const uint8_t * args, uint8_t args_size) {
-	ESP_LOGI(LOG_CONTROLLER, "Address = %04X; args size = %d; args[0] == %02X", address, args_size, args[0]);
-return;
-	switch(args[0]) {
-	case CONTROLLER_HM_FULL_OPEN:
-		stepper_execute_command(STEPPER_COMMAND_FULL_OPEN);
-		break;
-	case CONTROLLER_HM_FULL_CLOSE:
-		stepper_execute_command(STEPPER_COMMAND_FULL_CLOSE);
-		break;
-	case CONTROLLER_HM_STEP_OPEN:
-		stepper_execute_command(STEPPER_COMMAND_STEP_OPEN);
-		break;
-	case CONTROLLER_HM_STEP_CLOSE:
-		stepper_execute_command(STEPPER_COMMAND_STEP_CLOSE);
-		break;
-	case CONTROLLER_HM_CALIBRATE:
-		stepper_execute_command(STEPPER_COMMAND_CALIBRATE);
-		break;
-	case CONTROLLER_HM_ON_CHARGE_BEGIN:
-	case CONTROLLER_OM_ON_CHARGE_BEGIN:
-		charger_confirm_started();
-		break;
-	case CONTROLLER_HM_ON_CHARGE_END:
-	case CONTROLLER_OM_ON_CHARGE_END:
-		charger_stop();
-		break;
-	case CONTROLLER_OM_STATUS_AIR:
-		controller_on_air_data(args[0] - 100, args[1], (args[2] << 16) + (args[3] << 8) + args[4]);
-		break;
-	case CONTROLLER_OM_STATUS_NOISE_ALRM:
-		controller_on_noise_alarm(args[0] == 1);
-		break;
-	case CONTROLLER_OM_STATUS_NOISE_DATA:
-		controller_on_noise_data(args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7]);
-		break;
+void controller_process_hm_command(uint8_t arg) {
+	switch (arg) {
+		case CONTROLLER_HM_FULL_OPEN:
+			stepper_execute_command(STEPPER_COMMAND_FULL_OPEN);
+			break;
+		case CONTROLLER_HM_FULL_CLOSE:
+			stepper_execute_command(STEPPER_COMMAND_FULL_CLOSE);
+			break;
+		case CONTROLLER_HM_STEP_OPEN:
+			stepper_execute_command(STEPPER_COMMAND_STEP_OPEN);
+			break;
+		case CONTROLLER_HM_STEP_CLOSE:
+			stepper_execute_command(STEPPER_COMMAND_STEP_CLOSE);
+			break;
+		case CONTROLLER_HM_CALIBRATE:
+			stepper_execute_command(STEPPER_COMMAND_CALIBRATE);
+			break;
+
+		case CONTROLLER_HM_ON_CHARGE_BEGIN:
+			charger_confirm_started();
+			break;
+		case CONTROLLER_HM_ON_CHARGE_END:
+			charger_stop();
+			break;
 	}
 }
 
+void controller_process_air_data(int8_t temperature, uint8_t humidity, uint32_t pressure) {
+	ESP_LOGI(LOG_CONTROLLER, "OM AIR status: temperature=%d C, humidity=%d%%, pressure=%li Pa", temperature, humidity, pressure);
+}
+
+void controller_process_om_noise_alarm(bool alarm) {
+	ESP_LOGI(LOG_CONTROLLER, "OM NOISE alarm: %s", (alarm ? "ON" : "OFF"));
+}
+
+void controller_process_noise_data(uint8_t freq1, uint8_t level1, uint8_t freq2, uint8_t level2, uint8_t freq3, uint8_t level3, uint8_t freq4, uint8_t level4, uint8_t freq5, uint8_t level5) {
+	ESP_LOGI(LOG_CONTROLLER, "OM NOISE data: freq1: [ %d = %d ]; freq2: [ %d = %d ]; freq3: [ %d = %d ]; freq4: [ %d = %d ]; freq5: [ %d = %d ]",
+			freq1, level1,
+			freq2, level2,
+			freq3, level3,
+			freq4, level4,
+			freq5, level5
+			);
+}
+
 void controller_process_command(const t_fm_command * command) {
-	if (command->address == FM_COMMAND_ADDRESS__PDU) {
-		controller_process_pdu_command(command->args[0]);
-	} else {
-		controller_process_internal_command(command->address, command->args, command->args_size);
+	const uint8_t * args = command->args;
+
+	switch (command->address) {
+	case FM_COMMAND_ADDRESS__PDU:
+		controller_process_pdu_command(args[0]);
+		break;
+	case FM_COMMAND_ADDRESS__MM_STEPPER_STATUS:
+		// ignore this address - I receive myself commands
+		break;
+	case FM_COMMAND_ADDRESS__OM_ALARM:
+		controller_process_om_noise_alarm(args[0]);
+		break;
+	case FM_COMMAND_ADDRESS__OM_AIR_STATUS:
+		controller_process_air_data(args[0] - 100,
+				                    args[1],
+									(args[2] << 16) + (args[3] << 8) + args[4]);
+		break;
+	case FM_COMMAND_ADDRESS__OM_NOISE_DATA:
+		controller_process_noise_data(args[0], args[1],
+									  args[2], args[3],
+									  args[4], args[5],
+									  args[6], args[7],
+									  args[8], args[9]);
+		break;
+	case FM_COMMAND_ADDRESS__HM_COMMAND:
+		controller_process_hm_command(args[0]);
+		break;
 	}
 }
 
@@ -109,17 +126,5 @@ void controller_on_status(uint8_t status) {
 	cmd.args_size = 1;
 
 	fm_sender_send(&cmd);
-}
-
-void controller_on_air_data(int8_t temperature, uint8_t humidity, uint32_t pressure) {
-	ESP_LOGI(LOG_CONTROLLER, "OM AIR status: temperature=%d C, humidity=%d%%, pressure=%li Pa", temperature, humidity, pressure);
-}
-
-void controller_on_noise_alarm(bool alarm) {
-	ESP_LOGI(LOG_CONTROLLER, "OM NOISE alarm: %s", (alarm ? "ON" : "OFF"));
-}
-
-void controller_on_noise_data(uint8_t freq1, uint8_t level1, uint8_t freq2, uint8_t level2, uint8_t freq3, uint8_t level3, uint8_t freq4, uint8_t level4) {
-	ESP_LOGI(LOG_CONTROLLER, "OM NOISE data: freq1: [ %d = %d ]; freq2: [ %d = %d ]; freq3: [ %d = %d ]; freq4: [ %d = %d ]", freq1, level1, freq2, level2, freq3, level3, freq4, level4);
 }
 
